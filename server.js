95d23798f33af6039a3d1cdbac475cdc7b8a55fb
Kurt Van Etten <kurt@vanetten.us>
2019-12-18 00:26:24 -0500

    Basic CRUD operations complete. Need to add error checking.  Seeds not populating.

--- server.js
+++ server.js
@@ -1,9 +1,74 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
 
-const server = express();
+const router = express.Router();
 
-server.use(express.json());
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await db('accounts');
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = server;
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    const account = await db('accounts')
+      .where('id', id)
+      .first();
+    res.json(account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget,
+    };
+    const [id] = await db('accounts').insert(payload);
+    const newAccount = await db('accounts')
+      .where('id', id)
+      .first();
+    res.json(newAccount);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget,
+    };
+    const id = Number.parseInt(req.params.id);
+    await db('accounts')
+      .where('id', id)
+      .update(payload);
+    const updatedAccount = await db('accounts')
+      .where('id', id)
+      .first();
+    res.json(updatedAccount);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    const lines = await db('accounts')
+      .where('id', id)
+      .del();
+    res.status(202).json({ 'records deleted': lines });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
